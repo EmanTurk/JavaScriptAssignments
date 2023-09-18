c9d8a0e5c2e1ee3ebd5991f10e0e7fcb345588e4
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-09-18 20:28:52 +0300

    Create 1.2_more_on_primitive_data_types.js

---
+++ 1.2/1.2_more_on_primitive_data_types.js
@@ -0,0 +1,15 @@
+var firstVariable = 50;
+
+var secondVariable = 30;
+secondVariable = 40; 
+
+var thirdVariable = "He's got it!";
+
+var restaurantBill = 100.50;
+
+var taxAmount = 10.22;
+
+var totalBill = restaurantBill + taxAmount;
+console.log("Your total bill is " + totalBill + " $.");
+
+Math.round(50.6);
