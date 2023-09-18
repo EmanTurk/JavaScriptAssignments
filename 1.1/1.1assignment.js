7eb7d60ff040ab2b924f69f5cde481fcdf2739a1
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-09-18 18:16:55 +0300

    first assignment

---
+++ 1.1/1.1assignment.js
@@ -0,0 +1,29 @@
+// Create a variable whose value holds a string.
+const stringValue = "Hello, World!";
+
+// Create a variable whose value holds a number.
+const numberValue = 42;
+
+// Create a variable whose value holds an object.
+const objectValue = { key: "value" };
+
+// Create a variable whose value holds a boolean.
+const booleanValue = true;
+
+// Create a variable whose value holds null.
+const nullValue = null;
+
+// Create a variable whose value is undefined.
+const undefinedValue = undefined;
+
+// Create a variable whose value holds a symbol.
+const symbolValue = Symbol("mySymbol");
+
+// Log the types of your variables.
+console.log("Type of stringValue:", typeof stringValue);
+console.log("Type of numberValue:", typeof numberValue);
+console.log("Type of objectValue:", typeof objectValue);
+console.log("Type of booleanValue:", typeof booleanValue);
+console.log("Type of nullValue:", typeof nullValue);
+console.log("Type of undefinedValue:", typeof undefinedValue);
+console.log("Type of symbolValue:", typeof symbolValue);
