4455b42f5c2e45851f2661bf3ba1a0d1da821f71
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-09-20 18:13:10 +0300

    code fixed

---
+++ 6.3/6.3exercise.js
@@ -0,0 +1,9 @@
+const random = ['homework', 'work', 'food', 'no time', 'sos'];
+
+const lengths = [];
+
+for (let i = 0; i < random.length; i++) {
+    lengths.push(random[i].length);
+}
+
+console.log(lengths);  
