5611a31948ca91199295a35f511a3be990e533a0
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-09-26 01:44:48 +0300

    done2.2
    basic funstions

---
+++ 2.2/2.2_functions.js
@@ -0,0 +1,7 @@
+function tellFortune(
+    jobTitle, location, partnerName, numberOfChildren){
+        return `You will be a ${jobTitle} in ${location} married to ${partnerName} with with ${numberOfChildren} children`;
+    }
+
+ 
+    console.log(tellFortune(myJobTitle, myLocation, myPartnerName, myNumberOfChildren));
