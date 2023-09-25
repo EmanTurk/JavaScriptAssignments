a07448f13c0cd66ef940d7d43d664de5f675d46d
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-09-25 16:50:28 +0300

    8.2 modified and tested

---
+++ 8.2/8.2_object methods.js
@@ -0,0 +1,21 @@
+const myCountry= {
+    countryname:'France',
+    Capital: 'Paris',
+    population: '40 million',
+    neighbours: ['Italy', 'UK', 'Belgium'],
+    
+    describe: function() {
+        return `${this.country} has ${this.population} people, their mother tongue is ${this.language}, they have ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`;
+    },
+    
+    checkIsland: function() {
+        this.isIsland = this.neighbours.length === 0 ? true : false;
+        return this.isIsland;
+    }
+};
+//okay try
+console.log(myCountry.describe());
+
+myCountry.checkIsland();
+console.log(myCountry.isIsland); 
+console.log(myCountry.isIsland);
