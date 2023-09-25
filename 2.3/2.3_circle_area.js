e90d371737412a37905d394bf094ab3e76e8bc86
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-09-26 01:58:37 +0300

    2.3-pi

---
+++ 2.3/2.3_circle_area.js
@@ -0,0 +1,9 @@
+function circleArea(radius) {
+    const area = Math.PI * Math.pow(radius, 2);
+    
+    console.log(`Area: ${area}`);
+
+    console.log(`Rounded Area: ${area.toFixed(2)}`);
+}
+//try
+circleArea(7);  
