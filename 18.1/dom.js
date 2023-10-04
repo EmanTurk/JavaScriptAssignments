929c9d7ff7d54a382656f4c316793922415e6d83
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-10-04 09:27:34 +0300

    18.1
    modified

--- 18.1/dom.js
+++ 18.1/dom.js
@@ -1,38 +1,39 @@
 //quistion 2:
   // Select and log each element using querySelector/querySelectorAll
-    var h1 = document.querySelector('h1');
-    console.log(h1);
+  const h1 = document.querySelector('h1');
+  console.log(h1);
 
-    var p = document.querySelector('p');
-    console.log(p);
+  const p = document.querySelector('p');
+  console.log(p);
 
-    var a = document.querySelector('a');
-    console.log(a);
+  const a = document.querySelector('a');
+  console.log(a);
 
-    var img = document.querySelector('img');
-    console.log(img);
+  const img = document.querySelector('img');
+  console.log(img);
 
-    var divWithClass = document.querySelector('.x');
-    console.log(divWithClass);
+  const divWithClass = document.querySelector('.x');
+  console.log(divWithClass);
 
-    var divWithId = document.querySelector('#y');
-    console.log(divWithId);
+  const divWithId = document.querySelector('#y');
+  console.log(divWithId);
 
-    // For multiple elements with the same tag, you might want to use querySelectorAll and loop through the NodeList
-    var inputs = document.querySelectorAll('input');
-    inputs.forEach(function(input) {
-      console.log(input);
-    });
-// question 3:
+  // For multiple elements with the same tag, you might want to use querySelectorAll and loop through the NodeList
+  const inputs = document.querySelectorAll('input');
+  inputs.forEach(function(input) {
+    console.log(input);
+  });
 
-//inside script:
-var inputElements1 = document.querySelectorAll('input');
-inputElements1.forEach(function(input) {
-  console.log(input);
-});
+// question 3:  // Using querySelectorAll
 
-// Using getElementsByTagName-here
-var inputElements2 = document.getElementsByTagName('input');
-Array.from(inputElements2).forEach(function(input) {
-  console.log(input);
-});
+
+  const inputElements1 = document.querySelectorAll('input');
+  inputElements1.forEach(function(input) {
+    console.log(input);
+  });
+
+  const inputElements2 = document.getElementsByTagName('input');
+  Array.from(inputElements2).forEach(function(input) {
+    console.log(input);
+  });
+
